56c1d9fc82352bd3bbc2cf19619ffb22b4b81589
sreya-sriram <91830477+sreya-sriram@users.noreply.github.com>
2025-04-13 21:43:25 +0530

    fixing rules layout

--- frontend.py
+++ frontend.py
@@ -4,12 +4,16 @@ import os
 import pandas as pd
 import re
 
-#Loading required files
-if not os.path.exists("association_rules.csv"):
-    file_id = "1D-A3WpSw8OtuVVM9BD1j8dLw3PORsQna" 
-    gdown.download(f"https://drive.google.com/uc?id={file_id}", "association_rules.csv", quiet=False)
+@st.cache_data
+def download_and_load_csv(id, output):
+    gdown.download(f"https://drive.google.com/uc?id={id}", output, quiet=False)
+    df = pd.read_csv(output)
+    return df
 
-rules_df = pd.read_csv("association_rules.csv")
+#Loading required files
+if not os.path.exists("rules.csv"):
+    rules_df = download_and_load_csv("1D-A3WpSw8OtuVVM9BD1j8dLw3PORsQna", "rules.csv")
+
 for col in ['antecedents', 'consequents']:
     rules_df[col] = rules_df[col].astype(str).apply(lambda x: re.sub(r"frozenset\(\{?(.*?)\}?\)", r"\1", x))
 
@@ -23,7 +27,7 @@ st.sidebar.markdown("An interactive dashboard for analyzing frequent itemsets, a
 
 #Association Rules Row
 st.subheader("Association Rules")
-c1, c2 = st.columns((3,7))
+c1, c2 = st.columns((2.5,7.5))
 with c1:
    conf_val = st.slider("Confidence", 0.5, 1.0, (0.5, 1.0), step=0.01)
    lift_val = st.slider("Lift", 1.0, round(float(rules_df['lift'].max()), 1), (1.0, round(float(rules_df['lift'].max()), 1)), step=0.1)
@@ -37,10 +41,21 @@ top_rules = filtered_rules.sort_values(by="confidence", ascending=False).head(50
 top_rules = top_rules[['antecedents', 'consequents', 'confidence', 'lift']]
 
 with c2:
-    if top_rules.empty:
-        st.warning("No rules match the selected filters!")
-    else:
-        st.table(top_rules.reset_index(drop=True))
+    with st.container():
+        st.markdown(
+            """
+            <div style='height: 400px; overflow-y: auto;'>
+            """,
+            unsafe_allow_html=True
+        )
+
+        if top_rules.empty:
+            st.warning("No rules match the selected filters!")
+        else:
+            st.table(top_rules.reset_index(drop=True))
+
+        st.markdown("</div>", unsafe_allow_html=True)
+    
 
 
 
